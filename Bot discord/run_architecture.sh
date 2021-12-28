603d1f3b19ec356dab267e3c4992f04135428c89
Nicolas Saldivia <64153626+Holy7o7@users.noreply.github.com>
2021-12-28 20:20:50 -0300

    update

--- Bot discord/run_architecture.sh
+++ Bot discord/run_architecture.sh
@@ -8,4 +8,6 @@ echo $DATABASE_IP
 
 DATABASE_IP=$DATABASE_IP docker-compose -f docker-compose.yml up -d --build birthday_manager
 
+docker-compose -f docker-compose.yml up -d --build youtube
+
 docker-compose -f docker-compose.yml up -d --build discord_listener
